0c949f5941afc729f137583840ff8eb2f35597e6
Alija Bobija <bobija.alija@gmail.com>
2019-06-03 22:26:20 +0200

    Initial code commit

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+ * This file is part of wiattend-server project
+ * https://github.com/abobija/wiattend-srv
+ */
+
+require('./server')
+	.start(8181)
---
+++ server.js
@@ -0,0 +1,44 @@
+/*
+ * This file is part of wiattend-server project
+ * https://github.com/abobija/wiattend-srv
+ */
+
+(function() {
+	var http = require('http');
+	
+	var _tag = function(rfidTagStr) {
+		if(rfidTagStr == null || rfidTagStr.length < (5 * 5 - 1)) {
+			return null;
+		}
+		
+		var bytes = rfidTagStr
+			.split (' ')
+			.filter(function(el) { return el.length != 0; });
+		
+		if(bytes.length != 5) {
+			return null;
+		}
+		
+		return bytes.join(' ');
+	};
+	
+	exports.start = function (port) {
+		var secretGuid = '2ce81521-c42f-4556-8c28-c69d7e3a3a47';
+		
+		http.createServer(function (req, res) {
+			if (req.method === 'POST' 
+				&& req.url === '/log' 
+				&& req.headers['sguid'] === secretGuid
+				&& req.headers['rfid-tag'] != null) {
+				
+				var tag = _tag(req.headers['rfid-tag']);
+				
+				if(tag != null) {
+					console.log(tag);
+				}
+			}
+			
+			res.end();
+		}).listen(port);
+	};
+})()
