4593d4ff8693c1b62200b7ba07462d9ed2393ded
Alija Bobija <bobija.alija@gmail.com>
2019-06-06 17:45:38 +0200

    Using express instead of http for serving

--- src/server.js
+++ src/server.js
@@ -4,54 +4,58 @@
  */
 
 (() => {
-	const http   = require('http');
+	const express = require('express');
 	const db     = require('mysql');
 	const config = require('./config');
 	const rfid   = require('./rfid');
-	
-	exports.start = () => {
-		http.createServer((req, res) => {
-			res.statusCode = 400;
+
+	const app = express();
+
+	app.post('/log', (req, res) => {
+		res.statusCode = 400;
+		
+		if (req.headers['sguid'] === '2ce81521-c42f-4556-8c28-c69d7e3a3a47'
+			&& req.headers['rfid-tag'] != null) {
 			
-			if (req.method === 'POST'
-				&& req.url === '/log'
-				&& req.headers['sguid'] === '2ce81521-c42f-4556-8c28-c69d7e3a3a47'
-				&& req.headers['rfid-tag'] != null) {
+			var rfidUid = rfid.tag(req.headers['rfid-tag']);
+			
+			if(rfidUid != null) {
+				console.log(rfidUid);
 				
-				var rfidUid = rfid.tag(req.headers['rfid-tag']);
+				var conn = db.createConnection(config.mysql);
 				
-				if(rfidUid != null) {
-					console.log(rfidUid);
+				conn.connect((err) => {
+					if(err) throw err;
 					
-					var conn = db.createConnection(config.mysql);
-					
-					conn.connect((err) => {
+					conn.query("SELECT t.*,"
+						+ " (SELECT COALESCE((SELECT l.`direction` FROM `log` l WHERE `tag_id` = t.id ORDER BY l.`id` DESC LIMIT 1) * -1, 1)) AS next_direction"
+						+ " FROM `tag` t WHERE t.`uid` = '" + rfidUid + "'", (err, tags) => {
 						if(err) throw err;
 						
-						conn.query("SELECT t.*,"
-							+ " (SELECT COALESCE((SELECT l.`direction` FROM `log` l WHERE `tag_id` = t.id ORDER BY l.`id` DESC LIMIT 1) * -1, 1)) AS next_direction"
-							+ " FROM `tag` t WHERE t.`uid` = '" + rfidUid + "'", (err, tags) => {
-							if(err) throw err;
+						if(tags.length == 0) {
+							res.end();
+						} else {
+							var tag = tags[0];
 							
-							if(tags.length == 0) {
-								res.end();
-							} else {
-								var tag = tags[0];
+							conn.query('INSERT INTO `log`(`tag_id`, `direction`) VALUES(' + tag.id + ', ' + tag.next_direction + ')', (err, insertLogResult)  => {
+								if(err) throw err;
 								
-								conn.query('INSERT INTO `log`(`tag_id`, `direction`) VALUES(' + tag.id + ', ' + tag.next_direction + ')', (err, insertLogResult)  => {
-									if(err) throw err;
-									
-									res.statusCode = 200;
-									res.write(JSON.stringify(tag));
-									res.end();
-								});
-							}
-						});
+								res.statusCode = 200;
+								res.write(JSON.stringify(tag));
+								res.end();
+							});
+						}
 					});
-				}
-			} else {
-				res.end();
+				});
 			}
-		}).listen(config.httpPort);
+		} else {
+			res.end();
+		}
+	});
+
+	exports.start = () => {
+		app.listen(config.httpPort, () => {
+			console.log('Server has been started.');
+		});
 	};
 })();
